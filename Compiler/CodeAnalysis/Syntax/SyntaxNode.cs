49223d54fbd1a43d53fc85bd3812616d8c76bcb7
Laslo Rajda <laslo.rajda@novalite.rs>
2024-05-04 20:57:25 +0200

    Moved writing closer to nodes

--- Compiler/CodeAnalysis/Syntax/SyntaxNode.cs
+++ Compiler/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -38,4 +38,37 @@ public abstract class SyntaxNode
             }
         }
     }
+
+    public void WriteTo(TextWriter writer) => PrettyPrint(writer, this);
+
+    public override string ToString()
+    {
+        using var writer = new StringWriter();
+        WriteTo(writer);
+        return writer.ToString();
+    }
+
+    private static void PrettyPrint(TextWriter writer, SyntaxNode node, string indent = "", bool isLast = true)
+    {
+        var marker = isLast ? "└──" : "├──";
+
+        writer.Write(indent);
+        writer.Write(marker);
+        writer.Write(node.Kind);
+
+        if (node is SyntaxToken t && t.Value != null)
+        {
+            writer.Write(" ");
+            writer.Write(t.Value);
+        }
+
+        writer.WriteLine();
+
+        indent += isLast ? "   " : "│  ";
+
+        var lastChild = node.GetChildren().LastOrDefault();
+
+        foreach (var child in node.GetChildren())
+            PrettyPrint(writer, child, indent, child == lastChild);
+    }
 }
--- MyCompiler/Program.cs
+++ MyCompiler/Program.cs
@@ -31,7 +31,7 @@ while (true)
     if (showTree)
     {
         Console.ForegroundColor = ConsoleColor.DarkGray;                
-        PrettyPrint(syntaxTree.Root);
+        syntaxTree.Root.WriteTo(Console.Out);
         Console.ResetColor();
     }
 
@@ -67,29 +67,4 @@ while (true)
 
         Console.WriteLine();
     }
-}
-
-
-static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true)
-{
-    var marker = isLast ? "└──" : "├──";
-
-    Console.Write(indent);
-    Console.Write(marker);
-    Console.Write(node.Kind);
-
-    if (node is SyntaxToken t && t.Value != null)
-    {
-        Console.Write(" ");
-        Console.Write(t.Value);
-    }
-
-    Console.WriteLine();
-
-    indent += isLast ? "   " : "│  ";
-
-    var lastChild = node.GetChildren().LastOrDefault();
-
-    foreach (var child in node.GetChildren())
-        PrettyPrint(child, indent, child == lastChild);
 }
