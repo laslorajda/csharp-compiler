011707aa8d0e3fda55bf94e1c24e66f45a6e8708
Laslo Rajda <laslo.rajda@novalite.rs>
2024-05-04 17:23:21 +0200

    Fake commit

---
+++ MyCompiler.Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -0,0 +1,13 @@
+﻿using Compiler.CodeAnalysis.Syntax;
+
+namespace MyCompiler.Tests.CodeAnalysis.Syntax;
+
+public class ParserTests
+{
+    
+    // [Theory]
+    // public void ParserBinaryExpressionHonorsPrecedences(SyntaxKind op1, SyntaxKind op2)
+    // {
+    //     
+    // }
+}
